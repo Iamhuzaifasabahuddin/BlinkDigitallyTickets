332eea0a85504c9752a9eeb2e2f0538c36af2651
Hexz <huzaifasabah@gmail.com>
2025-11-19 13:06:05 -0500

    Added Personal Reminders for PMs & Printing Reminder

--- reminder.py
+++ reminder.py
@@ -1,9 +1,10 @@
 import os
 from collections import defaultdict
+
+import pandas as pd
+from notion_client import Client
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
-from notion_client import Client
-import pandas as pd
 
 bot = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 
@@ -11,6 +12,7 @@ notion_token = os.environ['NOTION_TOKEN']
 notion = Client(auth=notion_token)
 
 DATABASE_ID = os.environ['NOTION_DATABASE_ID']
+
 
 def fetch_tickets_from_notion():
     """Fetch all tickets from Notion database with pagination."""
@@ -27,7 +29,7 @@ def fetch_tickets_from_notion():
                 )
             else:
                 results = notion.data_sources.query(data_source_id=DATABASE_ID,
-                                                 sorts=[{"timestamp": "created_time", "direction": "descending"}])
+                                                    sorts=[{"timestamp": "created_time", "direction": "descending"}])
 
             for page in results["results"]:
                 props = page["properties"]
@@ -44,7 +46,8 @@ def fetch_tickets_from_notion():
                     "Priority": props["Priority"]["select"]["name"] if props["Priority"]["select"] else "Medium",
                     "Date Submitted": props["Date Submitted"]["date"]["start"] if props["Date Submitted"][
                         "date"] else "",
-                    "Submitted Time": props["Submitted Time"]["rich_text"][0]["text"] ["content"] if props["Submitted Time"]["rich_text"] else "",
+                    "Submitted Time": props["Submitted Time"]["rich_text"][0]["text"]["content"] if
+                    props["Submitted Time"]["rich_text"] else "",
                     "Created By": props["Created By"]["select"]["name"] if props["Created By"]["select"][
                         "name"] else "",
                     "Assigned To": props["Assigned To"]["select"]["name"] if props["Assigned To"]["select"][
@@ -52,7 +55,8 @@ def fetch_tickets_from_notion():
                     "Resolved Date": props["Resolved Date"]["date"]["start"] if props.get("Resolved Date") and
                                                                                 props["Resolved Date"][
                                                                                     "date"] else None,
-                    "Resolved Time": props["Resolved Time"]["rich_text"][0]["text"] ["content"] if props["Resolved Time"]["rich_text"] else "",
+                    "Resolved Time": props["Resolved Time"]["rich_text"][0]["text"]["content"] if
+                    props["Resolved Time"]["rich_text"] else "",
                     "Comments": props["Comments"]["rich_text"][0]["text"]["content"] if props["Comments"][
                         "rich_text"] else "",
                 }
@@ -65,7 +69,7 @@ def fetch_tickets_from_notion():
 
         if not df.empty:
             if "Date Submitted" in df.columns:
-                df["Date Submitted"] = pd.to_datetime(df["Date Submitted"],format="%Y-%m-%d", errors='coerce')
+                df["Date Submitted"] = pd.to_datetime(df["Date Submitted"], format="%Y-%m-%d", errors='coerce')
             if "Resolved Date" in df.columns:
                 df["Resolved Date"] = pd.to_datetime(df["Resolved Date"], format="%Y-%m-%d", errors='coerce')
 
@@ -77,12 +81,15 @@ def fetch_tickets_from_notion():
 
         ticket_list = defaultdict(list)
         printed_list = defaultdict(list)
+        personal_list = defaultdict(list)
         for name in combined:
-            tickets = df[(df["Created By"] == name) | (df["Assigned To"] == name)]
-            printed = tickets.copy()
-            tickets = tickets[-tickets["Issue"].str.contains("Printed", case=False, na=False)]
+            tickets = df[
+                ((df["Created By"] == name) | (df["Assigned To"] == name)) &
+                (df["Created By"] != df["Assigned To"])
+                ]
 
-            tickets = tickets[~tickets["Issue"].str.contains("Printed", case=False, na=False)]
+            printed = tickets.copy()
+            tickets = tickets[-tickets["Issue"].str.contains("Printed|Complimentary|Proof", case=False, na=False)]
 
             ticket_list[name].append(tickets["ID"].tolist())
             ticket_list[name].append(tickets["Issue"].astype(str).tolist())
@@ -91,10 +98,16 @@ def fetch_tickets_from_notion():
             printed_list[name].append(printed["ID"].tolist())
             printed_list[name].append(printed["Issue"].astype(str).tolist())
 
-        return combined, ticket_list, printed_list
+            personal = df[df["Created By"] == df["Assigned To"]]
+            personal = personal[personal["Created By"] == name]
+            personal_list[name].append(personal["ID"].tolist())
+            personal_list[name].append(personal["Issue"].astype(str).tolist())
+
+        return combined, ticket_list, printed_list, personal_list
     except Exception as e:
         print(e)
         return pd.DataFrame()
+
 
 def get_user_id_by_email(email):
     try:
@@ -116,7 +129,6 @@ def send_dm(user_id, message):
 
 
 if __name__ == '__main__':
-    import pprint
 
     names = {
         "Aiza Ali": "aiza.ali@topsoftdigitals.pk",
@@ -134,32 +146,64 @@ if __name__ == '__main__':
         "Elishba": "elishba@topsoftdigitals.pk",
         "Shahrukh Yousuf": "shahrukh.yousuf@topsoftdigitals.pk",
         "Hassan Siddiqui": "hassan.siddiqui@topsoftdigitals.pk",
+        "Huzaifa Sabah Uddin": "huzaifa.sabah@topsoftdigitals.pk",
         "Farman Ali": "farmanali@topsoftdigitals.pk"
     }
 
-    name_list, ticket_dict, printed_dict = fetch_tickets_from_notion()
-    name_list = [name for name in name_list if name != "Huzaifa Sabah Uddin"]
+    name_list, ticket_dict, printed_dict, personal_dict = fetch_tickets_from_notion()
     hexz_id = get_user_id_by_email("huzaifa.sabah@topsoftdigitals.pk")
 
     for name in name_list:
+        tickets_exists: bool = False
+        personal_exists: bool = False
         if name not in ticket_dict:
             continue
 
-        tickets, issues = ticket_dict[name]
-        tickets_2, printings = printed_dict[name]
+        tickets, issues = ticket_dict.get(name, ([], [])) if name != "Huzaifa Sabah Uddin" else ([], [])
+        tickets_3, personal = personal_dict.get(name, ([], []))
         id_ = get_user_id_by_email(names.get(name))
 
+        if tickets:
+            ticket_lines = "\n".join([f"{t}: {i}" for t, i in zip(tickets, issues)])
+            tickets_exists = True
+        else:
+            ticket_lines = ""
 
-        ticket_lines = "\n".join([f"{t}: {i}" for t, i in zip(tickets, issues)])
-        # printed_lines = "\n".join([f"{t}: {i}" for t, i in zip(tickets_2, printings)])
+        if tickets_3:
+            personal_lines = "\n".join([f"{t}: {i}" for t, i in zip(tickets_3, personal)])
+            personal_exists = True
+        else:
+            personal_lines = ""
+
+        if tickets_exists:
+            message = (
+                f"🔔 *Reminder for:* *<@{id_}>*\n\n"
+                f"Here are your open tickets:\n"
+                f"{ticket_lines}\n\n"
+                f"‼ Please provide an update to *<@{hexz_id}>* or update it on the app when possible. 📝"
+            )
+            send_dm(id_, message)
+
+        if tickets_exists:
+            send_dm(hexz_id, f"🚀 Notification sent to *<@{id_}>*!")
+
+        if personal_exists:
+            message = (
+                f"🔔 *Personal Reminder for:* *<@{id_}>*\n\n"
+                f"Here are your personal tickets reminders:\n"
+                f"{personal_lines}\n\n"
+            )
+            send_dm(id_, message)
+
+    tickets_2, printings = printed_dict.get("Huzaifa Sabah Uddin", ([], []))
+    if tickets_2:
+        printed_lines = "\n".join([f"{t}: {i}" for t, i in zip(tickets_2, printings)])
         message = (
-            f"🔔 *Reminder for:* *<@{id_}>*\n"
-            f"Here are your open tickets:\n"
-            f"{ticket_lines}\n\n"
-            f"‼ Please provide an update to *<@{hexz_id}>* or update it on the app when possible. 📝"
-        )
+            f"🔔 *Printing Reminder for:* *<@{hexz_id}>*\n\n"
+            f"Pending Prints:\n"
+            f"{printed_lines}\n\n"
 
-        send_dm(id_, message)
-        send_dm(hexz_id, f"🚀 Notification sent to *<@{id_}>*!")
+        )
+        send_dm(hexz_id, message)
 
     send_dm(hexz_id, "🔔 Reminder: Check your open tickets!")
